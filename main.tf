84e0100d277dce3e68198191ee44d15a39041a19
Rohan <rgrohangupta65@gmail.com>
2023-11-29 03:35:11 +0530

    Roboshop component

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
-#resource "aws_docdb_cluster" "docdb" {
-#  cluster_identifier      = "${var.env}-docdb"
-#  engine                  = var.engine
-#  master_username         = data.aws_ssm_parameter.user.value
-#  master_password         = data.aws_ssm_parameter.pass.value
-#  backup_retention_period = var.backup_retention_period
-#  preferred_backup_window = var.preferred_backup_window
-#  skip_final_snapshot     = true
-#}
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${var.env}-docdb"
+  engine                  = var.engine
+  master_username         = data.aws_ssm_parameter.user.value
+  master_password         = data.aws_ssm_parameter.pass.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = true
+  db_subnet_group_name = aws_docdb_subnet_group.main.name
+  kms_key_id = data.aws_kms_key.key.key_id
+}
 
 resource "aws_docdb_subnet_group" "main" {
   name       = "${var.env}-docdb"
