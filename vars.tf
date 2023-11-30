c43600a28c037b38cec459957bf8a16dd951dae4
Rohan <rgrohangupta65@gmail.com>
2023-11-30 18:14:41 +0530

    Roboshop component

--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
-variable "env" {}
-variable "engine" {}
-variable "backup_retention_period" {}
-variable "preferred_backup_window" {}
-variable "skip_final_snapshot" {}
-variable "engine_version" {}
-variable "subnet_ids" {}
-variable "tags" {}
-variable "storage_encrypted" {
-  default = true
-}
-variable "no_of_instances" {}
-variable "instance_class" {}
+#variable "env" {}
+#variable "engine" {}
+#variable "backup_retention_period" {}
+#variable "preferred_backup_window" {}
+#variable "skip_final_snapshot" {}
+#variable "engine_version" {}
+#variable "subnet_ids" {}
+#variable "tags" {}
+#variable "storage_encrypted" {
+#  default = true
+#}
+#variable "no_of_instances" {}
+#variable "instance_class" {}
